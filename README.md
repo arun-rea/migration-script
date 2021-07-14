bdac6831bb5536896e3c2557b2f1601d7d0b0fab
Arun Surendran <arun.surendran@rea-group.com>
2021-07-14 15:15:29 +0800

    Added delete repo functionality to the script

--- README.md
+++ README.md
@@ -4,4 +4,10 @@ This is a simple migration script to clone all the repositories from source GitH
 ## How to run
 - Clone the repo into the server or local machine using the command `git clone git@github.com:arun-rea/migration-script.git`
 - open the directory `cd migration-script`
-- Execute the command `./git-migration.sh <usertoken> <maximum pages> <repository count in each page>`
+- Add execution privilege to the script file `chmod +x git-migration.sh`
+- Execute the command `./git-migration.sh <source user token> <maximum pages> <repository count in each page>`
+
+## Things to note
+
+- Inorder to execute this script, you need to create personal access tokens from the REA GHE and as well as PG GitHub
+- GHE token (source) needs to be passed via the command line, where the PG Token (destination) is hardedcode in the script.
--- git-migration.sh
+++ git-migration.sh
@@ -19,11 +19,34 @@ else
     repos=$3
 fi
 
-# starting page number
+# starting page counter
 page=1
 
 # Personal token of destination Github [this may need to change later]
-dest_token=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
+dest_token=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
+
+# helper function to clean up if any existing docker containers and images
+function delete_repo(){
+    if [ -z "$1" ]; then
+        echo "Pass authorization token"
+        exit 1
+    else
+        token=$1
+    fi
+
+    if [ -z "$2" ]; then
+        echo "Pass the repository name to delete"
+        exit 1
+    else
+        repo_name=$2
+    fi
+    # delete repository
+    curl -X DELETE \
+         -H "Authorization: token $token" \
+         -H "Accept: application/vnd.github.v3+json" \
+         https://api.github.com/repos/propertyguru/"$repo_name" \
+         || true
+}
 
 until [ $max -lt $page ];do
 
@@ -35,22 +58,36 @@ until [ $max -lt $page ];do
 
         #get the repo name
         filename=${basename%.*}
+
         # push the mirror to the new GitHub repository
-        cd $basename
-        # create new repository in destination github account.
-        # curl -H "Authorization: token $dest_token" --data "{\"name\":\"$filename\",\"private\":\"true\"}" https://api.github.com/orgs/propertyguru/repos
-        # curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $dest_token" --data "{\"org\":\"propertyguru\",\"team_slug\":\"iproperty\",\"repo\":\"$filename\",\"permission\":\"maintain\",\"owner\":\"propertyguru\"}" https://api.github.com/orgs/propertyguru/teams/iproperty/repos/propertyguru/$filename
+        cd "$basename" || exit
+
+        # delete repo if already present in the destination github account
+        delete_repo "$dest_token" "$filename"
+
+        # create new repository in destination github account based on organisation
         curl -H "Authorization: token $dest_token" \
-            --data "{\"name\":\"$filename\",\"private\":\"true\"}" \
-            https://api.github.com/user/repos
+             --data "{\"name\":\"$filename\",\"private\":\"true\"}" \
+             https://api.github.com/orgs/propertyguru/repos
+        curl -X PUT \
+             -H "Accept: application/vnd.github.v3+json" \
+             -H "Authorization: token $dest_token" \
+             --data "{\"org\":\"propertyguru\",\"team_slug\":\"iproperty\",\"repo\":\"$filename\",\"permission\":\"maintain\",\"owner\":\"propertyguru\"}" \
+             https://api.github.com/orgs/propertyguru/teams/iproperty/repos/propertyguru/"$filename"
+        
+        # create new repository in destination github account based on user
+        # curl -H "Authorization: token $dest_token" \
+        #    --data "{\"name\":\"$filename\",\"private\":\"true\"}" \
+        #    https://api.github.com/user/repos
             
         #push the repository to destination account
-        git push --mirror git@github.com:arun-rea/$basename
+        git push --mirror git@github.com:propertyguru/"$basename"
 
         # remove temporary local repo
         cd ..
-        echo $(pwd)
-        rm -rf $basename
+        rm -rf "$basename"
     done
     let page++
 done
+
+
