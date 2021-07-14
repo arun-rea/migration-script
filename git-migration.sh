14aa99b0b6289a81413fdc60b1e2c65f4fd2b601
Arun Surendran <arun.surendran@rea-group.com>
2021-07-14 12:12:31 +0800

    Added support to park under the IPP team

--- git-migration.sh
+++ git-migration.sh
@@ -39,7 +39,11 @@ until [ $max -lt $page ];do
         cd $basename
         # create new repository in destination github account.
         # curl -H "Authorization: token $dest_token" --data "{\"name\":\"$filename\",\"private\":\"true\"}" https://api.github.com/orgs/propertyguru/repos
-        curl -H "Authorization: token $dest_token" --data "{\"name\":\"$filename\",\"private\":\"true\"}" https://api.github.com/user/repos
+        # curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $dest_token" --data "{\"org\":\"propertyguru\",\"team_slug\":\"iproperty\",\"repo\":\"$filename\",\"permission\":\"maintain\",\"owner\":\"propertyguru\"}" https://api.github.com/orgs/propertyguru/teams/iproperty/repos/propertyguru/$filename
+        curl -H "Authorization: token $dest_token" \
+            --data "{\"name\":\"$filename\",\"private\":\"true\"}" \
+            https://api.github.com/user/repos
+            
         #push the repository to destination account
         git push --mirror git@github.com:arun-rea/$basename
 
